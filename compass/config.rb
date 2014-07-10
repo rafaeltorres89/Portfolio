86de16ff96ab3f00e0a575f5386fe7af88d93726
Rafael Torres <MacRafa@MacBook-Pro-de-Rafael.local>
2014-07-09 23:43:46 -0300

    Novo modelo ux designer
    Porttólio Online versão 1.0

---
+++ compass/config.rb
@@ -0,0 +1,17 @@
+# Compass configuration file.
+
+# We also support plugins and frameworks, please read the docs http://docs.mixture.io/preprocessors#compass
+# Susy (susy.oddbird.net) is available in Mixture, just uncomment to require and use
+# require 'susy'
+project_path = File.expand_path("..",File.dirname(__FILE__))
+
+# Important! change the paths below to match your project setup
+
+css_dir = "assets/style" # update to the path of your css files.
+sass_dir = "assets/sass" # update to the path of your sass files.
+images_dir = "assets/image" # update to the path of your image files.
+javascripts_dir = "assets/script" # update to the path of your script files.
+
+line_comments = false
+cache = false
+
